2333cfe5ed348aa0c7232f611294295065c8dd68
roula.m <roula.misrabi@kibarsoft.com>
2019-01-02 22:48:55 +0100

    roll back added notification name

--- Applozic/Kibarsoft/Extensions.swift
+++ Applozic/Kibarsoft/Extensions.swift
@@ -15,14 +15,10 @@ public extension Notification.Name {
     
     static var ALChatViewDidAppear: Notification.Name {
         return .init(rawValue: "Kibarsoft.Applozic.ALChatViewDidAppear")
-    }
-    
-    //This value MUST match the one defined in ALMessageService.h && ALChatViewController.m
-    static let AL_NEW_MESSAGE_NOTIFICATION = NSNotification.Name("newMessageNotification")    
+    }    
 }
 
 @objc extension NSNotification {
     public static var ALChatViewWillAppear = Notification.Name.ALChatViewWillAppear
     public static var ALChatViewDidAppear = Notification.Name.ALChatViewDidAppear
-    public static var AL_NEW_MESSAGE_NOTIFICATION = Notification.Name.AL_NEW_MESSAGE_NOTIFICATION
 }
